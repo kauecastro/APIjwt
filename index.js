4fec67bd27a736ac3d56858693e505fbeef1b4f3
Kaue Castro <kaue.castro@era-br.com>
2017-06-02 12:18:39 -0300

    Authentication

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var app = require('./config/custom-express')();
-require('./config/custom-database')('kauecastro@ds161931.mlab.com:61931/apijwt');
+require('./config/custom-database')('kauecastro:Zener47kaue@ds161931.mlab.com:61931/apijwt');
 
 app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000');
